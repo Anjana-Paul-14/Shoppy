e561cd49ff2ad13cf9ca3c38e7ac1ceb315a563a
Anjana Paul <anjanapaul0614@gmail.com>
2023-12-14 23:38:15 +0530

    added folders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './App.css';
 
 const App =() => {
   return (
-    <div>
-    APP
-    </div>
+    <h1 className='underline text-3x1'>APP</h1>
+    
+
+
+    
+    
+    
   );
 }
 
