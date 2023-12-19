a807507d23053cf729225a9a5ca27618314a7e3a
Anjana Paul <anjanapaul0614@gmail.com>
2023-12-19 12:09:11 +0530

    NavBar - imported pakages and components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { FiShoppingCart } from 'react-icons/fi';
+import { BsChatLeft } from 'react-icons/bs'; 
+import { RiNotification3Line } from 'react-icons/ri';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import avatar from '../data/avatar.jpg';
+import { Cart, Chat, Notification, UserProfile } from '.';
+import { useStateContext } from '../contexts/ContextProvider';
+
+
 
 const Navbar = () => {
+  const {activeMenu, setActiveMenu} = useStateContext();
+
   return (
-    <div>
+    <div className='flex justify-between p-2 md:ml-6 md:mr-6 relative'>
       
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
