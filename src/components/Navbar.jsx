4e6c8ee3eba6245307db9141685de725bb655656
Anjana Paul <anjanapaul0614@gmail.com>
2023-12-21 19:46:40 +0530

    added NavButton

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,6 +16,7 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       onClick={() => customFunc()}
       style={{ color }}
       className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+       
     >
       <span
         style={{ background: dotColor }}
@@ -31,7 +32,7 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between p-2 md:ml-6 md:mr-6 relative'>
-      
+      <NavButton title='Menu' customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color="blue" icon={<AiOutlineMenu/>}/>
     </div>
   );
 };
