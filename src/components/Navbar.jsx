cbbfd343e0351a312e4311cad0563c4dec8c9920
Anjana Paul <anjanapaul0614@gmail.com>
2023-12-21 20:06:12 +0530

    added chat icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,6 +35,7 @@ const Navbar = () => {
       <NavButton title='Menu' customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color="blue" icon={<AiOutlineMenu/>}/>
       <div className='flex'>
       <NavButton title='Cart' customFunc={() => handleClick('cart')} color="blue" icon={<FiShoppingCart/>}/>
+      <NavButton title='Chat' dotColor='#03C9D7'customFunc={() => handleClick('chat')} color="blue" icon={<BsChatLeft/>}/>
       </div>
     </div>
   );
