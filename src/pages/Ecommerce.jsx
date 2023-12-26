16bfe3b16338042c066fd8bb97ca2c1ae1e4e7de
Anjana Paul <anjanapaul0614@gmail.com>
2023-12-26 12:22:38 +0400

    import in Ecommerce.jsx

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import {GoPrimitiveDot } from 'react-icons/go';
+import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
+import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
+import product9 from '../data/product9.jpg';
+import { IoIosMore } from 'react-icons/io';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
 
 const Ecommerce = () => {
   return (
