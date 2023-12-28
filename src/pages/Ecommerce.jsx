91b8e5b8cc420cce97a0d67782e12e1fc4601f5d
Anjana Paul <anjanapaul0614@gmail.com>
2023-12-28 12:15:25 +0400

    added classname

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -75,8 +75,8 @@ const Ecommerce = () => {
                 <div className=" border-r-1 border-color m-4 pr-10" >
                   <div>
                     <p>
-                      <span>$93,438</span>
-                      <span>23%</span>
+                      <span className="text-3xl font-semibold">$93,438</span>
+                      <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">23%</span>
                     </p>
                   </div>
                 </div>
