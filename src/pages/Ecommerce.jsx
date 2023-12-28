d5b08581a06dc8288360fe7fc84f441dd78169cc
Anjana Paul <anjanapaul0614@gmail.com>
2023-12-28 12:16:42 +0400

    created another p tag for budget

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -78,6 +78,7 @@ const Ecommerce = () => {
                       <span className="text-3xl font-semibold">$93,438</span>
                       <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">23%</span>
                     </p>
+                    <p>Budget</p>
                   </div>
                 </div>
               </div>
