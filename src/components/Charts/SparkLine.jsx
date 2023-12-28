976c828cc4a35fbc4faee51349a21911d91f5dcc
Anjana Paul <anjanapaul0614@gmail.com>
2023-12-28 13:10:32 +0400

    added graph in sparkline

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -3,8 +3,30 @@ import { SparklineComponent, Inject, SparklineTooltip  } from '@syncfusion/ej2-r
 
 const SparkLine = ({id, height, width, color, data, type, currentColor}) => {
   return (
-    <SparklineComponent>
-      
+    <SparklineComponent
+    id={id}
+    height={height}
+    width={width}
+    lineWidth={1}
+    valueType="Numeric"
+    fill={color}
+    border={{ color: currentColor, width: 2 }}
+    tooltipSettings={{
+      visible: true,
+    //   // eslint-disable-next-line no-template-curly-in-string
+      format: '${x} : data ${y}',
+      trackLineSettings: {
+        visible: true,
+      },
+    }}
+    // markerSettings={{ visible: ['All'], size: 2.5, fill: currentColor }}
+    dataSource={data}
+    xName="x"
+    yName="y"
+    type={type}
+    >
+       <Inject services={[SparklineTooltip]} />
+ 
     </SparklineComponent>
   )
 }
