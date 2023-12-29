c88f2f3321f497d5f626485d7692c47de482f638
Anjana Paul <anjanapaul0614@gmail.com>
2023-12-29 17:10:41 +0400

    added search bar by using toolbar in employees.jsx

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GridComponent, ColumnsDirective, ColumnDirective, Page, Inject, Search } from '@syncfusion/ej2-react-grids';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Inject, Search, Toolbar } from '@syncfusion/ej2-react-grids';
 import { employeesData, employeesGrid } from '../data/dummy';
 import { Header } from '../components';
 
@@ -13,11 +13,12 @@ const Employees = () => {
         id="gridcomp"
         dataSource={employeesData}
         allowPaging
-        allowSorting>
+        allowSorting
+        toolbar={['Search']}>
         <ColumnsDirective>
         {employeesGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
         </ColumnsDirective>
-        <Inject services={[Search, Page]}/>
+        <Inject services={[Search, Page, Toolbar]}/>
       </GridComponent>
     </div>
   );
