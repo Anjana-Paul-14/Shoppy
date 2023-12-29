1dc6a56859f090ac38e87e3f7ed64b6df2316ca3
Anjana Paul <anjanapaul0614@gmail.com>
2023-12-29 16:46:42 +0400

    added properties in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-const Header = () => {
+const Header = ({category, title}) => {
   return (
     <div>Header</div>
   )
-}
+};
 
-export default Header
+export default Header;
