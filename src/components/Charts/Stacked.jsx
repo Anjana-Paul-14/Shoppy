d241549ca4e385f302b9caa722484178dec9c441
Anjana Paul <anjanapaul0614@gmail.com>
2024-01-04 14:52:47 +0530

    dashboard completed

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React from 'react';
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
+
 import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const Stacked = ({ width, height }) => {
+  const { currentMode } = useStateContext();
+
   return (
-    <ChartComponent 
-      width={width}
-      height={height}
-      id ='charts'
+    <ChartComponent
+      id="charts"
       primaryXAxis={stackedPrimaryXAxis}
       primaryYAxis={stackedPrimaryYAxis}
+      width={width}
+      height={height}
       chartArea={{ border: { width: 0 } }}
       tooltip={{ enable: true }}
-      // background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
       legendSettings={{ background: 'white' }}
-      >
+    >
       <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
       <SeriesCollectionDirective>
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
